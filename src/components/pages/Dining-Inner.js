070a0c7fb290f5b48a54b35a97f95834458b3a29
arslan-prism <arslan.prism@gmail.com>
2021-01-12 17:20:48 +0500

    added dining inner page with responsiveness

---
+++ src/components/pages/Dining-Inner.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react'
+import Headertwo from '../layouts/Headertwo';
+import Footertwo from '../layouts/Footertwo';
+import Mainbanner from '../sections/homepage-two/Banner';
+import Bookingform from '../sections/homepage-two/Bookingform';
+import BottomNavigator from '../sections/homepage-two/BottomNavigator';
+import DiningTitleBlock from '../sections/dining/main-text-block';
+import DiningGrid from '../sections/dining/dining-grid';
+import DiningOfferSlider from '../sections/dining/dining-offer-sldier';
+import DiningInnerTitleBlock from '../sections/dining-inner/main-text-block';
+import DiningInnerInfo from '../sections/dining-inner/dining-grid';
+import OtherRecommendations from '../sections/dining/dining-grid-item';
+
+const roomsData = [
+  {
+    title: "Paris Sychelles Restaurant",
+    link: "",
+    linkText: "View More",
+    description: "Guests can hide themseleves away in these comfortable rooms located in the middle of a main buillding set to the rear of hotel.",
+    image: require('./../../assets/img/room-suites/room1.jpg')
+  },
+  {
+    title: "Lecardinal Restaurant",
+    link: "",
+    linkText: "View More",
+    description: "Guests can hide themseleves away in these comfortable rooms located in the middle of a main buillding set to the rear of hotel.",
+    image: require('./../../assets/img/room-suites/room1.jpg')
+  },
+  {
+    title: "Superior Room",
+    link: "",
+    linkText: "View More",
+    description: "Guests can hide themseleves away in these comfortable rooms located in the middle of a main buillding set to the rear of hotel.",
+    image: require('./../../assets/img/room-suites/room1.jpg')
+  },
+]
+
+const offersData = [
+  {
+    title: "Family Suite Garden View",
+    link: "",
+    linkText: "View More",
+    description: "Guests can hide themseleves away in these comfortable rooms located in the middle of a main buillding set to the rear of hotel.",
+    image: require('./../../assets/img/room-suites/suite1.jpg')
+  },
+  {
+    title: "Family Suite Garden View",
+    link: "",
+    linkText: "View More",
+    description: "Guests can hide themseleves away in these comfortable rooms located in the middle of a main buillding set to the rear of hotel.",
+    image: require('./../../assets/img/room-suites/suite1.jpg')
+  },
+  {
+    title: "Family Suite Garden View",
+    link: "",
+    linkText: "View More",
+    description: "Guests can hide themseleves away in these comfortable rooms located in the middle of a main buillding set to the rear of hotel.",
+    image: require('./../../assets/img/room-suites/suite1.jpg')
+  },
+  {
+    title: "Family Suite Garden View",
+    link: "",
+    linkText: "View More",
+    description: "Guests can hide themseleves away in these comfortable rooms located in the middle of a main buillding set to the rear of hotel.",
+    image: require('./../../assets/img/room-suites/suite1.jpg')
+  },
+  {
+    title: "Family Suite Garden View",
+    link: "",
+    linkText: "View More",
+    description: "Guests can hide themseleves away in these comfortable rooms located in the middle of a main buillding set to the rear of hotel.",
+    image: require('./../../assets/img/room-suites/suite1.jpg')
+  },
+]
+
+class DiningInner extends Component {
+  render() {
+    return (
+      <div className="bg-white dining-inner-wrapper">
+        <Headertwo />
+        {/*====== BANNER PART START ======*/}
+        <Mainbanner title={"Paris Seychelles Restaurant"} />
+        {/*====== BANNER PART ENDS ======*/}
+        {/*====== BOOKING FORM START ======*/}
+        <Bookingform />
+        {/*====== BOOKING FORM END ======*/}
+        {/*====== TITLE START ======*/}
+        <DiningInnerTitleBlock title={"Paris Seychelles Restaurant"} />
+        {/*====== TITLE END ======*/}
+        {/*====== ROOM GRID START ======*/}
+        <DiningInnerInfo />
+        {/*====== ROOM GRID END ======*/}
+        {/*====== SUITES GRID START ======*/}
+        <DiningOfferSlider title={"Offers"} data={offersData} />
+        {/*====== SUITES GRID END ======*/}
+        {/*====== OTHERS GRID START ======*/}
+        <OtherRecommendations title={"Other Restaurants & Bars"} data={roomsData} />
+        {/*====== OTHERS GRID END ======*/}
+        <Footertwo />
+
+        <BottomNavigator />
+      </div>
+    );
+  }
+}
+
+export default DiningInner;
---
+++ src/components/sections/dining-inner/dining-grid.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+const DiningInnerInfo = (props) => {
+  return (
+    <div className='dining-info-wrapper'>
+      <div className='container'>
+        <div className="row">
+          <div className="col-12 col-md-4">
+            <div className="travellers-choice">
+              <div className="image-wrapper">
+                <img src={require('./../../../assets/img/travellers-choice.png')} alt="" />
+              </div>
+              <p className="info-title">opening hours</p>
+              <p className="text-muted">Daily: <br /> 7pm - 11pm</p>
+              <p className="text-muted font-italic">*Advance dinner reservations required</p>
+            </div>
+          </div>
+          <div className="col-12 col-md-4">
+            <div className="travellers-choice">
+              <div className="image-wrapper">
+                <img src={require('./../../../assets/img/travellers-choice.png')} alt="" />
+              </div>
+              <p className="info-title">dress code</p>
+              <p className="text-muted">Daily: <br /> 7pm - 11pm</p>
+              <p className="text-muted font-italic">*Advance dinner reservations required</p>
+            </div>
+          </div><div className="col-12 col-md-4">
+            <div className="menu-buttons">
+              <button className="main-btn btn-filled">Download our Menu</button>
+              <button className="main-btn btn-filled">Download Beverage Menu</button>
+              <button className="main-btn btn-filled">Download wine list</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DiningInnerInfo;
