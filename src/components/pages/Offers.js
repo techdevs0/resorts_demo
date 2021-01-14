f16a310814e2970c3e935245b3e787e123c8a4f0
arslan-prism <arslan.prism@gmail.com>
2021-01-14 16:26:32 +0500

    added offers page

--- src/components/pages/Offers.js
+++ src/components/pages/Offers.js
@@ -1,25 +1,134 @@
 import React, { Component } from 'react'
-import Footer from '../layouts/Footertwo';
-import Breadcrumb from '../sections/offers/Breadcrumb';
-import Offer from '../sections/offers/Offers';
-import Featureroom from '../sections/offers/Featureroom';
 import Headertwo from '../layouts/Headertwo';
+import Footertwo from '../layouts/Footertwo';
+import Mainbanner from '../sections/homepage-two/Banner';
+import Bookingform from '../sections/homepage-two/Bookingform';
+import BottomNavigator from '../sections/homepage-two/BottomNavigator';
+import Subscribe from '../sections/common/Subscribe';
+import OfferTitleBlock from '../sections/offers/main-text-block';
+import OfferGrid from '../sections/offers/offer-grid';
+
+const roomsData = [
+  {
+    title: "Paris Sychelles Restaurant",
+    link: "",
+    linkText: "View More",
+    description: "Guests can hide themseleves away in these comfortable rooms located in the middle of a main buillding set to the rear of hotel.",
+    image: require('./../../assets/img/room-suites/room1.jpg')
+  },
+  {
+    title: "Lecardinal Restaurant",
+    link: "",
+    linkText: "View More",
+    description: "Guests can hide themseleves away in these comfortable rooms located in the middle of a main buillding set to the rear of hotel.",
+    image: require('./../../assets/img/room-suites/suite1.jpg')
+  },
+  {
+    title: "Superior Room",
+    link: "",
+    linkText: "View More",
+    description: "Guests can hide themseleves away in these comfortable rooms located in the middle of a main buillding set to the rear of hotel.",
+    image: require('./../../assets/img/room-suites/room1.jpg')
+  },
+  {
+    title: "Superior Room",
+    link: "",
+    linkText: "View More",
+    description: "Guests can hide themseleves away in these comfortable rooms located in the middle of a main buillding set to the rear of hotel.",
+    image: require('./../../assets/img/room-suites/suite1.jpg')
+  },
+  {
+    title: "Paris Sychelles Restaurant",
+    link: "",
+    linkText: "View More",
+    description: "Guests can hide themseleves away in these comfortable rooms located in the middle of a main buillding set to the rear of hotel.",
+    image: require('./../../assets/img/room-suites/room1.jpg')
+  },
+  {
+    title: "Lecardinal Restaurant",
+    link: "",
+    linkText: "View More",
+    description: "Guests can hide themseleves away in these comfortable rooms located in the middle of a main buillding set to the rear of hotel.",
+    image: require('./../../assets/img/room-suites/room1.jpg')
+  },
+  {
+    title: "Superior Room",
+    link: "",
+    linkText: "View More",
+    description: "Guests can hide themseleves away in these comfortable rooms located in the middle of a main buillding set to the rear of hotel.",
+    image: require('./../../assets/img/room-suites/suite1.jpg')
+  },
+  {
+    title: "Superior Room",
+    link: "",
+    linkText: "View More",
+    description: "Guests can hide themseleves away in these comfortable rooms located in the middle of a main buillding set to the rear of hotel.",
+    image: require('./../../assets/img/room-suites/room1.jpg')
+  },
+]
+
+const offersData = [
+  {
+    title: "Family Suite Garden View",
+    link: "",
+    linkText: "View More",
+    description: "Guests can hide themseleves away in these comfortable rooms located in the middle of a main buillding set to the rear of hotel.",
+    image: require('./../../assets/img/room-suites/suite1.jpg')
+  },
+  {
+    title: "Family Suite Garden View",
+    link: "",
+    linkText: "View More",
+    description: "Guests can hide themseleves away in these comfortable rooms located in the middle of a main buillding set to the rear of hotel.",
+    image: require('./../../assets/img/room-suites/suite1.jpg')
+  },
+  {
+    title: "Family Suite Garden View",
+    link: "",
+    linkText: "View More",
+    description: "Guests can hide themseleves away in these comfortable rooms located in the middle of a main buillding set to the rear of hotel.",
+    image: require('./../../assets/img/room-suites/suite1.jpg')
+  },
+  {
+    title: "Family Suite Garden View",
+    link: "",
+    linkText: "View More",
+    description: "Guests can hide themseleves away in these comfortable rooms located in the middle of a main buillding set to the rear of hotel.",
+    image: require('./../../assets/img/room-suites/suite1.jpg')
+  },
+  {
+    title: "Family Suite Garden View",
+    link: "",
+    linkText: "View More",
+    description: "Guests can hide themseleves away in these comfortable rooms located in the middle of a main buillding set to the rear of hotel.",
+    image: require('./../../assets/img/room-suites/suite1.jpg')
+  },
+]
 
 class Offers extends Component {
   render() {
     return (
-      <div>
+      <div className="bg-white">
         <Headertwo />
-        {/*====== BREADCRUMB PART START ======*/}
-        <Breadcrumb/>
-        {/*====== BREADCRUMB PART END ======*/}
-        <Offer/>
-        {/*====== FEATURE ROOM START ======*/}
-        <Featureroom/>
-        {/*====== FEATURE ROOM END ======*/}
-        <Footer />
-      </div>
+        {/*====== BANNER PART START ======*/}
+        <Mainbanner title={"Offers"} />
+        {/*====== BANNER PART ENDS ======*/}
+        {/*====== BOOKING FORM START ======*/}
+        <Bookingform />
+        {/*====== BOOKING FORM END ======*/}
+        {/*====== TITLE START ======*/}
+        <OfferTitleBlock />
+        {/*====== TITLE END ======*/}
+        {/*====== ROOM GRID START ======*/}
+        <OfferGrid title={null} data={roomsData} />
+        {/*====== ROOM GRID END ======*/}
 
+        <Subscribe />
+
+        <Footertwo />
+
+        <BottomNavigator />
+      </div>
     );
   }
 }
---
+++ src/components/sections/offers/main-text-block.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const OfferTitleBlock = (props) => {
+  return (
+    <div className="offer-title-block">
+      <div className="container">
+        <h2>Offers</h2>
+        <p>
+          Offering countless experience with outstanding services. Lorem ipsum doler set amet. 
+          Offering countless experience with outstanding services. Lorem ipsum doler set amet.
+          Offering countless experience with outstanding services. Lorem ipsum doler set amet.
+          Offering countless experience with outstanding services. Lorem ipsum doler set amet.
+      </p>
+      </div>
+    </div>
+  );
+}
+
+export default OfferTitleBlock;
