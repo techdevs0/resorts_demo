0ee218483974c3a38feb17d28cae8c5a96646348
Prism-me <bilal@prism-me.com>
2021-01-21 15:52:03 +0400

    css changs

--- src/components/sections/rooms-inner/room-amentities.js
+++ src/components/sections/rooms-inner/room-amentities.js
@@ -8,7 +8,7 @@ const RoomAmenities = (props) => {
         <ul className="room-features-list">
           <li><a href="#"><i className="fal fa-bath" />Air Conditioning</a></li>
           <li><a href="#"><i className="fal fa-shower" />Bathroom Amenities </a></li>
-          <li><a href="#"><i className="fal fa-steam" />Chrome Cast </a></li>
+          <li><a href="#"><i className="fal fa-key" />Chrome Cast </a></li>
           <li><a href="#"><i className="fal fa-wifi" />Free Wifi </a></li>
           <li><a href="#"><i className="fal fa-lock" />In-room safe </a></li>
           <li><a href="#"><i className="fal fa-lock" />Hairdryer</a></li>
