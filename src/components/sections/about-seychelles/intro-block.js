c69466144b9f0726f2b6e2ed3eb9869369e9fc5c
Prism-me <bilal@prism-me.com>
2021-01-21 12:58:38 +0400

    css and images changes

--- src/components/sections/about-seychelles/intro-block.js
+++ src/components/sections/about-seychelles/intro-block.js
@@ -7,7 +7,7 @@ const SeychellesIntroBlock = (props) => {
         <h2 className="text-center main-title">About Seychelles</h2>
         <div className="d-flex flex-wrap title-flex">
           <div className="intro-item">
-            <h3 className=" mr-sm-4">About Seychelles</h3>
+            <h3 className=" mr-sm-4">Call of the Paradise</h3>
             
             <p className="text-justify mr-sm-4">
             Seychelles is a blend of 115 coralline and granitic islands situated between 4 and 10 degrees south of equator and a thousand miles away from the east coast of Africa. It is known as the island paradise and is full of sandy beaches and pristine oceans, and lush green forests. The island nation is famed especially for honeymoon travels and it is no less than a heavenly kingdom for travelers seeking adventure. It is full of unspoiled and surreal locations including forests, beaches, and plains. It also houses a number of rare animal and plant species.</p>
