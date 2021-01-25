bd0b171163ce64a4046e358997085dc003e4933c
arslan-prism <arslan.prism@gmail.com>
2021-01-25 18:29:52 +0500

    added faq page

--- src/components/sections/faq/intro-block.js
+++ src/components/sections/faq/intro-block.js
@@ -1,16 +1,57 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { AccordionContext, Card, useAccordionToggle } from 'react-bootstrap';
+import Accordion from 'react-bootstrap/Accordion';
+import FAQCategories from './faq-categories';
+
+
+function ContextAwareToggle({ children, eventKey, callback }) {
+  const currentEventKey = useContext(AccordionContext);
+
+  // const decoratedOnClick = useAccordionToggle(
+  //   eventKey,
+  //   () => callback && callback(eventKey),
+  // );
+
+  const isCurrentEventKey = currentEventKey === eventKey;
+
+  return (
+    <h6 style={{padding:'0.5rem 0'}}> <i className={`pr-4 fa ${isCurrentEventKey ? 'fa-minus' : 'fa-plus'}`} /> 
+      {children}
+    </h6>
+  );
+}
 
 const FAQIntroBlock = (props) => {
+
+  const [currentIndex, setIndex] = useState(0);
+
+  const setCurrentIndex = (index) => {
+    setIndex(index);
+  }
+
   return (
     <div className="intro-block">
       <div className="container">
         <h2 className="text-center main-title">Frequently Asked Questions (F.A.Q's)</h2>
-        {/* <div className="intro-item">
-          <p className="mb-2">
-            Here are the 
-          </p>
-        </div> */}
-        
+        <FAQCategories setCurrentIndex={setCurrentIndex} currentIndex={currentIndex} />
+        <Accordion>
+          {
+            props.faqList?.map((x, i) => (
+              <Card>
+                <Accordion.Toggle as={Card.Header} eventKey={`${i}`} style={{ cursor: 'pointer' }} >
+                  <ContextAwareToggle eventKey={`${i}`} >
+                    {x.question}
+                  </ContextAwareToggle>
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey={`${i}`}>
+                  <Card.Body>
+                    <p>{x.answer}</p>
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            ))
+          }
+        </Accordion>
       </div>
     </div>
   );
