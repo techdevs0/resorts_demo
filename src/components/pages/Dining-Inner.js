9b92bd50b8ddeddf2f4fb7498ea3941237e969b2
Prism-me <bilal@prism-me.com>
2021-02-07 15:08:08 +0400

    faqs rooms and suites

--- src/components/pages/Dining-Inner.js
+++ src/components/pages/Dining-Inner.js
@@ -81,32 +81,108 @@ const offersData = [
 ]
 
 const faqList = [
-  {
-    question: 'How much does a wedding for 150 guests in Seychelles cost?',
-    answer: `Depending on how much you plan to spend, a wedding in Seychelles will cost somewhere between $1200 to $10000. Seychelles is home to some of the best wedding resort venues.`,
-    category: 'dining',
-    id: 15,
+  //sunset
+  { id : 17,
+    question: 'What is there to do in Seychelles at night?',
+    answer: `One prospect can be to dance all night in one of the many night clubs in Seychelles. Thanks to its tropical nature, the city allows guests to sit outside and enjoy the ocean breeze with a hint of lively music`,
+    category: 'dining'
   },
   {
-    question: 'Can foreigners get married in Seychelles?',
-    answer: `Yes, foreigners can get married in Seychelles. The documents need to be submitted 30 days prior to the wedding for verification. It is not necessary for the couple to be present for verification, it can be arranged beforehand. All the documents must be in English or french. If any of the documents are not in English or French they will have to be translated into either language. These documents will then have to be stamped and signed by a solicitor stating that it is the true translation of the original.`,
-    category: 'policy',
-    id: 16,
+    id:17,
+    question: 'Can you drink alcohol in Seychelles?',
+    answer: `Hotels and bars in Seychelles are allowed to serve alcohol while having a license. It is not advisable to drink in public places so the tourists should be careful and considerate of the local culture.`,
+    category:'policy'
   },
-  {
-    question: 'Can you get married in Seychelles?',
-    answer: `Yes, you can. Seychelles has numerous luxury wedding venues. Couples marrying in Seychelles must have a valid passport and birth certificate. ... Ceremonies are conducted by the Registrar per Seychelles Law. The date selected is subject to the Registrar's availability and the ceremony can be arranged Monday - Friday. Additional cost will be incurred if you plan to organize the wedding ceremony over the weekend or public holidays depending on the availability.`,
-    category: 'policy',
-    id: 17,
+  { id:17 ,
+    question: 'Is Seychelles a party place?',
+    answer: `Seychelles is not considered to be a party place and it is not known for its nightlife. Tourists will have to settle for calm and chill evenings. There aren’t many hotels and Casinos though there are some options but mostly hotels and resorts take this responsibility of entertaining guests.`,
+    category:'policy'
   },
 
-  {
-    question: 'Do the hotels have the facility to organize Couple Weddings? ',
-    answer: `Yes, hotels and resorts in Seychelles have the facility to organize couple weddings and the prices start from 850 Euros depending on the venue.`,
-    category: 'policy',
-    id: 18,
+  { id:17,
+    question: 'What are the best areas to stay in Seychelles?',
+    answer: `Here are some areas suited for different kinds of tourists. Victoria is the best place to go for first-timers. Beau Vallon is one of the most visited places in Seychelles. Praslin is the place to go for tourists looking for Nightlife. La Digue is considered to be the coolest place to go and Mahe is the best place to be for families.`,
+    category:'policy'
   },
-]
+//paris seychelles
+  { id:15,
+    question: 'Is it expensive to eat out in Seychelles?',
+    answer: `There are numerous restaurants in Seychelles, Victoria and the cost of eating out can vary. However, the average cost of lunch and dinner is SCR 560 per day. Breakfasts are usually a bit cheaper.`,
+    category:'policy'
+  },
+
+  { id:15,
+    question: 'What is Seychelles known for?',
+    answer: `Seychelles is known for its deep blue waters and yet unspoiled shores. However, there is much more to Seychelles than just a remote island including fine dining restaurants and luxurious resorts.`,
+    category:'policy'
+  },
+
+  { id:15,
+    question: 'How many days are enough to explore Seychelles?',
+    answer: `The Island is around 17 miles long. Scheduling three nights and four days would allow tourists to explore enough attractions including beaches, bars, and restaurants in Seychelles.`,
+    category:'policy'
+  },
+
+//lecocoloba
+{ id:16,
+  question: 'Is Seychelles expensive to eat?',
+  answer: `The prices at Seychelles bars and restaurants can vary. There are numerous options to choose from and there is no end to luxury so visitors need to be careful and spend according to their budget.`,
+  category:'policy'
+},
+
+{ id:16,
+  question: 'Is Seychelles better than the Maldives?',
+  answer: `Seychelles is comparatively larger and there is much more to explore than the Maldives. Moreover, Seychelles is home to some of the rarest species of fauna and flora. Having some of the most beautiful diving sites, it is a paradise for divers.  Mahé is the main island and it is home to Seychelles' best restaurants and hotels.`,
+  category:'policy'
+},
+
+{ id:16,
+  question: 'What are Seychelles best bars and clubs?',
+  answer: `Here is a list of some of the famous bars and clubs in Seychelles.
+  Club Liberte Casino
+  Beach Shak
+  Bar Ocean View
+  Honesty Bar
+  Rogan’s Irish Bar
+  Katiolo Night Club
+  Trader Vicks
+  1502
+  `,
+  category:'policy'
+},
+{ id:16,
+  question: 'Where in Seychelles can we enjoy nightlife?',
+  answer: `One of the best places to enjoy the nightlife in Seychelles is Katiolo. Due to its tropical nature, the open-air party vibe is amazing. `,
+  category:'policy'
+},
+
+//le cardinal
+
+{ id:18,
+  question: 'Is veg food available in Seychelles?',
+  answer: `There are a number of Seychelle restaurants that are vegetarian friendly, mainly in Mahe islands and La Digue. Some of the vegetarian-friendly restaurants include Rey and Josh Café, Mi Mum’s Takeaway, Les Rochers, and Mimi’s Café.`,
+  category:'policy'
+},
+
+{ id:18,
+  question: 'Is Seychelles expensive to eat?',
+  answer: `Prices can vary due to several factors. Primarily due to a huge number of fine dining restaurants in Seychelles However, the average price per day is somewhere around SCR 569. Dinner is considered to be expensive as compared to breakfast. There is something for every price range in the city.`,
+  category:'policy'
+},
+
+{ id:18,
+  question: 'What foods do they eat in Seychelles?',
+  answer: `Some of the common staple foods including fish, shellfish dishes, and seafood dishes are famous and are eaten with rice. Curry dishes with rice are generally well-liked. Seafood is cooked in different ways such as steamed, grilled, baked, and salted.`,
+  category:'policy'
+},
+{ id:18,
+  question: 'How much money do I need for Seychelles?',
+  answer: `According to visitors who have spent some time in Seychelles, the average daily budget should be around $105. Though it varies and numerous factors need to be taken into consideration. However, tourists should at least keep this amount in mind.`,
+  category:'policy'
+},
+
+];
+
 
 let breadcrumbItems = [
   {
--- src/components/pages/Rooms-Inner.js
+++ src/components/pages/Rooms-Inner.js
@@ -40,23 +40,18 @@ const roomsData = [
 
 const faqList = [
   {
-    question: 'How much does a wedding for 150 guests in Seychelles cost?',
-    answer: `Depending on how much you plan to spend, a wedding in Seychelles will cost somewhere between $1200 to $10000. Seychelles is home to some of the best wedding resort venues.`,
+    question: 'What is the difference between a deluxe and a superior room?',
+    answer: `There is not much of a difference in terms of area. However, the difference is in the availability of amenities. deluxe rooms are equipped with more amenities and usually are placed on higher floors with better views and surroundings.`,
     category: 'dining',
     id: 1,
   },
   {
-    question: 'Can foreigners get married in Seychelles?',
-    answer: `Yes, foreigners can get married in Seychelles. The documents need to be submitted 30 days prior to the wedding for verification. It is not necessary for the couple to be present for verification, it can be arranged beforehand. All the documents must be in English or french. If any of the documents are not in English or French they will have to be translated into either language. These documents will then have to be stamped and signed by a solicitor stating that it is the true translation of the original.`,
+    question: 'How do I choose the best hotel superior rooms in Seychelles?',
+    answer: `The choices for rooms and suites in Seychelles are endless and sometimes it gets hard to choose which one is the best. The key is to know that the difference is almost always in amenities and views. Try to gather information about which ones have more to offer in terms of amenities and always go for the ones with a better view.`,
     category: 'policy',
-    id: 7,
+    id: 1,
   },
-  {
-    question: 'Can you get married in Seychelles?',
-    answer: `Yes, you can. Seychelles has numerous luxury wedding venues. Couples marrying in Seychelles must have a valid passport and birth certificate. ... Ceremonies are conducted by the Registrar per Seychelles Law. The date selected is subject to the Registrar's availability and the ceremony can be arranged Monday - Friday. Additional cost will be incurred if you plan to organize the wedding ceremony over the weekend or public holidays depending on the availability.`,
-    category: 'policy',
-    id: 2,
-  },
+
   {
     question: 'Do the hotels have the facility to organize Couple Weddings? ',
     answer: `Yes, hotels and resorts in Seychelles have the facility to organize couple weddings and the prices start from 850 Euros depending on the venue.`,
@@ -64,22 +59,109 @@ const faqList = [
     id: 3,
   },
   {
-    question: 'Do the hotels have the facility to organize Couple Weddings? ',
-    answer: `Yes, hotels and resorts in Seychelles have the facility to organize couple weddings and the prices start from 850 Euros depending on the venue.`,
+    question: 'What does a two-bedroom suite mean?',
+    answer: `Contrary to the popular belief, A two bedroom suite generally means one bedroom and a living room. It does not mean two full bedrooms as some people might think. `,
     category: 'policy',
-    id: 19,
+    id: 21,
   },
   {
-    question: 'Do the hotels have the facility to organize Couple Weddings? ',
+    question: 'Which hotels in Seychelles have suites with a private balcony?',
+    answer: `There are many Seychelles Resorts and Hotels providing suites with private balconies. Fishermans Cove Resort and STORY Seychelles Resort are two of the best options to choose from. Both the resorts provide private balconies with great views of the Indian Ocean.
+    `,
+    category: 'policy',
+    id: 21,
+  },
+  {
+    question: 'What is the difference between a room and a suite?',
+    answer: `A room in a hotel is one single room with all the necessary amenities in it and an attached bathroom. However, a suite has a larger area with a small living room and sometimes along with a dining area as well.`,
+    category: 'policy',
+    id: 21,
+  },
+  {
+    question: 'Do the hotels have the facility to organize Couple Weddings?',
     answer: `Yes, hotels and resorts in Seychelles have the facility to organize couple weddings and the prices start from 850 Euros depending on the venue.`,
     category: 'policy',
     id: 20,
   },
   {
-    question: 'Do the hotels have the facility to organize Couple Weddings? ',
-    answer: `Yes, hotels and resorts in Seychelles have the facility to organize couple weddings and the prices start from 850 Euros depending on the venue.`,
+    question: 'Which hotels in Seychelles have suites with a private balcony?',
+    answer: `There are many Seychelles Resorts and Hotels providing suites with private balconies. Some of the options are listed here. Fishermans Cove Resort has the finest rooms with a private balcony which provides thrilling views of the Indian Ocean. The balcony is the perfect place to just sit and chill for a while away from the hustle and bustle of the city.`,
     category: 'policy',
-    id: 21,
+    id: 20,
+  },
+  {
+    question: 'What is the difference between a room and a suite?',
+    answer: `A room in a hotel is one single room with all the necessary amenities in it and an attached bathroom. However, a suite has a larger area with a small living room and sometimes along with a dining area as well.`,
+    category: 'policy',
+    id: 20,
+  },
+
+  {
+    question: 'What is a junior suite room? ',
+    answer: `Junior suites are usually smaller than the regular suites with no solid separation between the room and the living area. Moreover, the living area is an extension of the room so it's not that big.`,
+    category: 'policy',
+    id: 2,
+  },
+  {
+    question: 'Whats the difference between a suite and a junior suite? ',
+    answer: `Junior suites are usually smaller than the regular suites with no solid separation between the room and the living area. Moreover, the living area is an extension of the room so there is less space.`,
+    category: 'policy',
+    id: 2,
+  },
+  {
+    question: 'Which are the hotels with the best rooms and suites in Seychelles?',
+    answer: `The choices for rooms and suites in Seychelles hotels are endless and the quality varies as well so you will easily be able to find the ones for your liking. Some of the resorts with the best rooms and suites in Seychelles STORY Seychelles Resort and Fisherman’s Cove Resort `,
+    category: 'policy',
+    id: 2,
+  },
+  {
+    question: 'Which hotels in Seychelles have the best suites?',
+    answer: `There are a number of options to choose from because Seychelles has some of the best resorts and hotels. Fishermans Cove Resort and STORY Seychelles Resort are two of the many options you can choose from. Both of these resorts in Seychelles have unmatched service and amenities.`,
+    category: 'policy',
+    id: 19,
+  },
+  {
+    question: 'Which hotels in Seychelles have suites with a private balcony?',
+    answer: `There are many Seychelles Resorts and Hotels providing suites with private balconies. Some of the options are listed here. Fishermans Cove Resort and STORY Seychelles Resort also have private balconies with unmatched views of the beach.`,
+    category: 'policy',
+    id: 19,
+  },
+  {
+    question: 'Which family resorts in Seychelles are romantic?',
+    answer: `There is a huge list of romantic family resorts in Seychelles. However, fishermans Cove Resort tops the list because of its location and the mesmerizing views it provides. The resort provides every all the necessary amenities with unmatched vervice.
+    `,
+    category: 'policy',
+    id: 3,
+  },
+  {
+    question: 'What is the difference between a room and a suite?',
+    answer: `A room in a hotel is one single room with all the necessary amenities in it and an attached bathroom. However, a suite has a larger area with a small living room and sometimes along with a dining area as well.`,
+    category: 'policy',
+    id: 3,
+  },
+  {
+    question: 'Which hotels in Seychelles have suites with a private balcony? ',
+    answer: `There are many Seychelles Resorts and Hotels providing suites with private balconies. Fishermans Cove Resort has many rooms and suites with private balconies for you to enjoy amazing views of the Indian Ocean. `,
+    category: 'policy',
+    id: 3,
+  },
+  {
+    question: 'Are there private pools with hotel rooms in Seychelles?',
+    answer: `Yes, some of the Seychelles hotels and resorts have private pools with rooms but you might not find this facility in every hotel. However, the choices are unlimited and there are a number of hotels and resorts in Seychelles providing the private pool facility.`,
+    category: 'policy',
+    id: 7,
+  },
+  {
+    question: 'Can you order food to your room at Fishermans Cove Resort?',
+    answer: `Yes, you can order food directly to your room and we have a great menu with numerous options to choose from.`,
+    category: 'policy',
+    id: 7,
+  },
+  {
+    question: 'What is the difference between an Oceanview room and an oceanfront room?',
+    answer: `An ocean view room means that you will be able to see the ocean from the window or the balcony. However, an oceanfront room means that your room will directly be facing the ocean. Oceanfront rooms generally provide the best views.`,
+    category: 'policy',
+    id: 7,
   },
 ]
 
