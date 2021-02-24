11728c4d29bff820115c9525ff40b4a1f130a205
Arslan Shahab <arslanshahab@gmail.com>
2021-02-24 15:28:48 +0500

    made lighbox to handle multiple images

--- src/components/sections/gallery/gallery-grid-item.js
+++ src/components/sections/gallery/gallery-grid-item.js
@@ -6,17 +6,17 @@ const GalleryGridItem = (props) => {
   const [showLightBox, setShowLightBox] = useState(false)
   return (
     <div className="gallery-grid-item-wrapper">
-      <div className="grid-item" onClick={()=> setShowLightBox(true)}>
-        <div className="gallery-title" style={{ backgroundImage: `url(${props.image})`, backgroundPosition:'center', backgroundSize:'cover' }}>
+      <div className="grid-item" onClick={() => props.toggleLightBox(props?.index)}>
+        <div className="gallery-title" style={{ backgroundImage: `url(${props.image})`, backgroundPosition: 'center', backgroundSize: 'cover' }}>
           {/* <h4>{props.title}</h4> */}
           <div className="slide-hover-overlay"></div>
         </div>
-        {
+        {/* {
           showLightBox && <Lightbox
-          mainSrc={props.image}
-          onCloseRequest={() => setShowLightBox(false)}
-        />
-        }
+            mainSrc={props.image}
+            onCloseRequest={() => setShowLightBox(false)}
+          />
+        } */}
       </div>
     </div>
   );
--- src/components/sections/gallery/gallery-grid.js
+++ src/components/sections/gallery/gallery-grid.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RoomGridItem from './gallery-grid-item';
+import Lightbox from 'react-image-lightbox';
 
 const GalleryGrid = (props) => {
+
+  const [mainSrc, setMainSrc] = useState(props.data?.[0]);
+  const [showLightBox, setShowLightBox] = useState(false)
+  const [photoIndex, setPhotoIndex] = useState(0)
+
   return (
     <div className='container'>
       {
@@ -9,11 +15,29 @@ const GalleryGrid = (props) => {
       }
       <div className='gallery-grid-wrapper'>
         {
-          props.data?.map(x => (
-            <RoomGridItem title={x.title} image={x.avatar} link={x.link} linkText={x.linkText} description={x.description} />
+          props.data?.map((x, i) => (
+            <RoomGridItem index={i} toggleLightBox={(index) => { setShowLightBox(!showLightBox); setPhotoIndex(index); setMainSrc(props.data?.[index]) }} title={x.title} image={x.avatar} link={x.link} linkText={x.linkText} description={x.description} />
           ))
         }
       </div>
+      {
+        showLightBox && <Lightbox
+          mainSrc={mainSrc?.avatar}
+          onCloseRequest={() => setShowLightBox(false)}
+          nextSrc={props.data[(photoIndex + 1) % props.data.length]?.avatar}
+          prevSrc={props.data[(photoIndex + props.data.length - 1) % props.data.length]?.avatar}
+          onMovePrevRequest={() => {
+            setPhotoIndex((photoIndex + props.data.length - 1) % props.data.length);
+            setMainSrc(props.data[(photoIndex + props.data.length - 1 ) % props.data.length]);
+          }
+          }
+          onMoveNextRequest={() =>{
+            setPhotoIndex((photoIndex + 1) % props.data.length);
+            setMainSrc(props.data[(photoIndex + 1) % props.data.length]);
+          }
+          }
+        />
+      }
     </div>
   );
 }
