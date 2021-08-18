320c39da5d5424205d9fa577c9bd78b448085069
prism-me <bilal@prism-me.com>
2021-08-18 17:12:43 +0400

    360 view fixed

--- src/components/pages/Rooms-Inner.js
+++ src/components/pages/Rooms-Inner.js
@@ -283,7 +283,7 @@ class RoomsInner extends Component {
                 {/*====== ROOM GRID END ======*/}
                 {/*====== ROOM 360 GRID START ======*/}
                 <RoomVR360
-                  image={this.state.singleRoom?.uploads?.find((x) => x["360_view"] === "0")?.avatar}
+                  image={this.state.singleRoom?.uploads?.find((x) => x["360_view"] === "1")?.avatar}
                 />
                 {/*====== ROOM 360 GRID END ======*/}
               </div>
